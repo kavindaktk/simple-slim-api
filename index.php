86dd1c40f1d251fedd92623c08701c272d009f48
kavindaktk <36921119+kavindaktk@users.noreply.github.com>
2021-09-15 20:38:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,34 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+$app->get('/get_phrase/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
 
     return $response;
 });
+
+$app->post('/create_phrase/data', function (Request $request, Response $response, $arg){
+
+    $_input = $request->getParsedBody();
+
+    $_data_1 = $_input['myPhrase'];
+    $rsp = array();
+
+    if(!empty($_data_1)){
+
+        $rsp["error"] = false;
+        $rsp['message'] = "hello my phrase is ".$_data_1;
+    }else{
+
+        $rsp["error"] = false;
+        $rsp['message'] = "you have not posted any data" ;
+    }
+
+    return $response
+        ->withStatus(201)
+        ->withJson($rsp);
+});
+
+
 $app->run();

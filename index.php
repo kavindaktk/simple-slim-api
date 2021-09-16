24cd1652b9045456faa475a5d8d38dd6c106f137
kavindaktk <36921119+kavindaktk@users.noreply.github.com>
2021-09-17 02:25:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,48 @@
 <?php
+
+#require 'Connectiondb.php'
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
+#require 'Connectiondb.php'
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
+
+
+
 $app->get('/get_phrase/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
 
     return $response;
 });
+
+
+$app = new \Slim\App;
+$app->get('/get_phrase1/{name}', function (Request $request, Response $response, array $args) {
+	    $name = $args['name'];
+	    $rsp["error"] = false;
+	    $conn = getConnection();
+	    $stmt = $conn->query("SELECT * FROM my_phrase");
+             $rsp['message'] = $stmt;
+		    return $response
+		            ->withStatus(201)
+		            ->withJson($rsp);
+});
+
+
+
+$app = new \Slim\App;
+
+
+$app->add(function ($req, $res, $next) {
+	    $response = $next($req, $res);
+	        return $response
+			            ->withHeader('Access-Control-Allow-Origin', 'http://3.86.67.120')
+			                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+				            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+}); 
+
 
 $app->post('/create_phrase/data', function (Request $request, Response $response, $arg){
 
@@ -22,7 +54,12 @@ $app->post('/create_phrase/data', function (Request $request, Response $response
     if(!empty($_data_1)){
 
         $rsp["error"] = false;
-        $rsp['message'] = "hello my phrase is ".$_data_1;
+	$conn = getConnection();
+	$sql = "INSERT INTO my_phrase(id,phrase) VALUES(?,?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute(['',$_data_1]);
+	$rsp["message"]= "success";
+
     }else{
 
         $rsp["error"] = false;
@@ -31,8 +68,19 @@ $app->post('/create_phrase/data', function (Request $request, Response $response
 
     return $response
         ->withStatus(201)
-        ->withJson($rsp);
+	->withJson($rsp);
 });
 
 
+
 $app->run();
+
+  function getConnection() {
+	 $dbhost="myapiinstance.cluster-codpc1xvsr06.us-east-1.rds.amazonaws.com";
+	 $dbuser="MyAPIInstance";
+	 $dbpass="MyAPIInstance";
+	 $dbname="MyAPIDatabase";
+	 $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);  
+	 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	 return $dbh;
+	    }
